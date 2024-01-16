cd88d6093c8f85094c25a3f3599862374d3563fe
kacem <elamrikacem@gmail.com>
2024-01-16 14:13:16 +0100

    fixed the migration

--- migrations/Version20240108094555.php
+++ migrations/Version20240108094555.php
@@ -20,12 +20,12 @@ final class Version20240108094555 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD photos JSON DEFAULT NULL');
+        $this->addSql('ALTER TABLE user DROP photos');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP photos');
+        $this->addSql('ALTER TABLE user ADD photos JSON DEFAULT NULL');
     }
 }
